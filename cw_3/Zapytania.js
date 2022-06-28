dd79afe9b0a9031d130bd35263ef7dbeabc22ddf
JacekGebo <96851392+JacekGebo@users.noreply.github.com>
2022-06-29 01:00:03 +0200

    Add files via upload

---
+++ cw_3/Zapytania.js
@@ -0,0 +1,29 @@
+Zad. 1
+MATCH (movie:Movie) RETURN movie
+
+Zad. 2
+MATCH (people:Person {name: "Hugo Weaving"})-[:ACTED_IN]->(Movies) RETURN people,Movies
+
+Zad. 3
+MATCH (people:Person {name:"Hugo Weaving"})-[:ACTED_IN]->(m)<-[:DIRECTED]-(directors) RETURN directors
+
+Zad. 4
+MATCH (people:Person {name:"Hugo Weaving"})-[:ACTED_IN]->(m)<-[:ACTED_IN]-(actors) RETURN actors
+
+Zad. 4
+MATCH (people:Person)-[ACTED_IN]-(:Movie {title: "The Matrix"}) RETURN people
+
+Zad. 5
+MATCH (a:Movie{name:"The Matrix"})<-[:ACTED_IN]-(a:Person)-[:ACTED_IN]->(m2:Movie) RETURN a, m2
+
+Zad. 6
+MATCH (actors)-[:ACTED_IN]->(movie:Movie) RETURN actors, count(distinct movie) 
+
+Zad. 7
+MATCH (a)-[:WROTE]->(m)<-[:DIRECTED]-(a) RETURN a, m.title
+
+Zad. 8
+MATCH (a:Person {name:"Hugo Weaving"})-[:ACTED_IN]->(m:Movie)<-[:ACTED_IN]-(b:Person {name:"Keanu Reeves"}) RETURN a, b, m
+
+Zad. 9
+
